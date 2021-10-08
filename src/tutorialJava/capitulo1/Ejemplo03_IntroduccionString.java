cba46395ae64380fb3fcc9ca324986f1b9371690
rafaelMunoz77 <rafael.munoz77@gmail.com>
2021-10-08 11:19:35 +0200

    Cambios en ejercicios del cap 1

--- src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
+++ src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
@@ -8,14 +8,14 @@ public class Ejemplo03_IntroduccionString {
 	 */
 	public static void main (String args[]) {
 		
-		String cadenaCarac = "Tutorial de Lenguaje de Programación Java 8."; // Creació de una cadena de caracteres
+		String cadenaCarac = "Tutorial de Lenguaje de ProgramaciÃ³n Java 8."; // CreaciÃ³ de una cadena de caracteres
 		 
 		System.out.println("Valor de cadenaCarac: " + cadenaCarac);  // Imprime una cadena de caracteres
 		System.out.println("Longitud de cadenaCarac: " + cadenaCarac.length()); // Imprime l alongitud de una cadena
-		System.out.println("Localización por caracteres: " + cadenaCarac.indexOf("Lenguaje")); // Índice en el que encontramos una cadena
-		System.out.println("Localización por índices: " + cadenaCarac.charAt(12)); // Carácter encontrado en el índice 12, empezando por 0 
+		System.out.println("LocalizaciÃ³n por caracteres: " + cadenaCarac.indexOf("Lenguaje")); // Ã�ndice en el que encontramos una cadena
+		System.out.println("LocalizaciÃ³n por Ã­ndices: " + cadenaCarac.charAt(12)); // CarÃ¡cter encontrado en el Ã­ndice 12, empezando por 0 
 		System.out.println("Uso de subcadenas: " + cadenaCarac.substring(12, 20)); // Obtener una subcadena dentro de una cadena
-		System.out.println("Uso de toLower: " + cadenaCarac.toLowerCase()); // Convertir una cadena de caracteres en minúscula
-		System.out.println("Uso de toUpper: " + cadenaCarac.toUpperCase()); // Convertir una cadena de caracteres en mayúscula.
+		System.out.println("Uso de toLower: " + cadenaCarac.toLowerCase()); // Convertir una cadena de caracteres en minÃºscula
+		System.out.println("Uso de toUpper: " + cadenaCarac.toUpperCase()); // Convertir una cadena de caracteres en mayÃºscula.
 	}
 }
