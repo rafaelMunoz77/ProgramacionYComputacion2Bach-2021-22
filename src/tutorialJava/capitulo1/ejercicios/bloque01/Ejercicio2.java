ded2320a185034e074cf2339a9dbfc3a95078483
rafaelMunoz77 <rafael.munoz77@gmail.com>
2021-10-15 11:04:08 +0200

    Ejercicio 2 del bloque 1 del capítulo 1

---
+++ src/tutorialJava/capitulo1/ejercicios/bloque01/Ejercicio2.java
@@ -0,0 +1,24 @@
+package tutorialJava.capitulo1.ejercicios.bloque01;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); // Consigo leer del teclado.
+		
+		System.out.println("Introduzca un número entero: ");
+		int numEntero = sc.nextInt();
+		System.out.println("El número entero es " + numEntero);
+		
+		System.out.println("Introduzca un número flotante: ");
+		float numFlotante = sc.nextFloat();
+		System.out.println("El número flotante es " + numFlotante);
+		
+		System.out.println("Introduzca un número double: ");
+		double numDouble = sc.nextDouble();
+		System.out.println("El número double es: " + numDouble);
+		
+	}
+
+}
