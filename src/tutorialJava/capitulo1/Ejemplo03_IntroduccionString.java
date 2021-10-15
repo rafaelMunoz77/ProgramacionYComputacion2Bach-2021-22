f4e11887113da05d4c8897e26836e0033307fd0d
rafaelMunoz77 <rafael.munoz77@gmail.com>
2021-10-15 11:31:06 +0200

    Ejemplo de uso de Strings en el capítulo 1

--- src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
+++ src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
@@ -8,14 +8,17 @@ public class Ejemplo03_IntroduccionString {
 	 */
 	public static void main (String args[]) {
 		
-		String cadenaCarac = "Tutorial de Lenguaje de ProgramaciÃ³n Java 8."; // CreaciÃ³ de una cadena de caracteres
-		 
-		System.out.println("Valor de cadenaCarac: " + cadenaCarac);  // Imprime una cadena de caracteres
-		System.out.println("Longitud de cadenaCarac: " + cadenaCarac.length()); // Imprime l alongitud de una cadena
-		System.out.println("LocalizaciÃ³n por caracteres: " + cadenaCarac.indexOf("Lenguaje")); // Ã�ndice en el que encontramos una cadena
-		System.out.println("LocalizaciÃ³n por Ã­ndices: " + cadenaCarac.charAt(12)); // CarÃ¡cter encontrado en el Ã­ndice 12, empezando por 0 
-		System.out.println("Uso de subcadenas: " + cadenaCarac.substring(12, 20)); // Obtener una subcadena dentro de una cadena
-		System.out.println("Uso de toLower: " + cadenaCarac.toLowerCase()); // Convertir una cadena de caracteres en minÃºscula
-		System.out.println("Uso de toUpper: " + cadenaCarac.toUpperCase()); // Convertir una cadena de caracteres en mayÃºscula.
+		String miCadenaDeTexto = "Mi clase de 2º de bachillerato";
+		String otraCadenaDeTexto = "Otra cadena, otraaaa!!!";
+		
+		System.out.println("Mi cadena de texto: " + miCadenaDeTexto);
+		System.out.println("Longitud de la cadena: " + miCadenaDeTexto.length());
+		System.out.println("Localizando el carácter i: " + miCadenaDeTexto.indexOf('i'));
+		System.out.println("Localizando el segundo carácter i: " + miCadenaDeTexto.indexOf('i', 2));
+		System.out.println("Localizando el último carácter e: " + miCadenaDeTexto.lastIndexOf('e'));
+		System.out.println("Subcadena: " + miCadenaDeTexto.substring(3));
+		System.out.println("Otra subcadena: " + miCadenaDeTexto.substring(3, 6));
+		System.out.println("En minúsculas: " + otraCadenaDeTexto.toLowerCase());
+		System.out.println("En mayúsculas: " + otraCadenaDeTexto.toUpperCase());
 	}
 }
