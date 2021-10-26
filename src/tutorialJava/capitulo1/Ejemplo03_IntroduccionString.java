4b94ecd6322adba6e350cef38d25d804e5a62d4d
Rafael Munoz <rafael.munoz77@gmail.com>
2021-10-26 09:43:17 +0200

    Capítulo 2

--- src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
+++ src/tutorialJava/capitulo1/Ejemplo03_IntroduccionString.java
@@ -8,17 +8,14 @@ public class Ejemplo03_IntroduccionString {
 	 */
 	public static void main (String args[]) {
 		
-		String miCadenaDeTexto = "Mi clase de 2º de bachillerato";
-		String otraCadenaDeTexto = "Otra cadena, otraaaa!!!";
-		
-		System.out.println("Mi cadena de texto: " + miCadenaDeTexto);
-		System.out.println("Longitud de la cadena: " + miCadenaDeTexto.length());
-		System.out.println("Localizando el carácter i: " + miCadenaDeTexto.indexOf('i'));
-		System.out.println("Localizando el segundo carácter i: " + miCadenaDeTexto.indexOf('i', 2));
-		System.out.println("Localizando el último carácter e: " + miCadenaDeTexto.lastIndexOf('e'));
-		System.out.println("Subcadena: " + miCadenaDeTexto.substring(3));
-		System.out.println("Otra subcadena: " + miCadenaDeTexto.substring(3, 6));
-		System.out.println("En minúsculas: " + otraCadenaDeTexto.toLowerCase());
-		System.out.println("En mayúsculas: " + otraCadenaDeTexto.toUpperCase());
+		String cadenaCarac = "Tutorial de Lenguaje de Programación Java 8."; // Creació de una cadena de caracteres
+		 
+		System.out.println("Valor de cadenaCarac: " + cadenaCarac);  // Imprime una cadena de caracteres
+		System.out.println("Longitud de cadenaCarac: " + cadenaCarac.length()); // Imprime l alongitud de una cadena
+		System.out.println("Localización por caracteres: " + cadenaCarac.indexOf("Lenguaje")); // Índice en el que encontramos una cadena
+		System.out.println("Localización por índices: " + cadenaCarac.charAt(12)); // Carácter encontrado en el índice 12, empezando por 0 
+		System.out.println("Uso de subcadenas: " + cadenaCarac.substring(12, 20)); // Obtener una subcadena dentro de una cadena
+		System.out.println("Uso de toLower: " + cadenaCarac.toLowerCase()); // Convertir una cadena de caracteres en minúscula
+		System.out.println("Uso de toUpper: " + cadenaCarac.toUpperCase()); // Convertir una cadena de caracteres en mayúscula.
 	}
 }
