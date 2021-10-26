c1b12924de4c1c34cce7da737b0706c3b503c3da
rafaelMunoz77 <rafael.munoz77@gmail.com>
2021-10-26 10:31:12 +0200

    Ejemplos del capítulo 2 - Condicionales

--- src/tutorialJava/capitulo2_condicionales/Ejemplo01_Condicionales.java
+++ src/tutorialJava/capitulo2_condicionales/Ejemplo01_Condicionales.java
@@ -1,8 +1,5 @@
 package tutorialJava.capitulo2_condicionales;
 
-import javax.swing.JOptionPane;
-
-import tutorialJava.Utils;
 
 
 public class Ejemplo01_Condicionales {
@@ -12,12 +9,12 @@ public class Ejemplo01_Condicionales {
 	 * 
 	 * @param args
 	 */
-	public static void main (String args[]) {		
+	public static void main (String args[]) {	
 //		primerEjemploCondicionalSimple();
 //		ejemploCondicionalCompuesto();
 //		ejemploCondicionalAnidado();
 //		ejemploCondicionalMultiple();
-//		ejemploOperadorTernario();
+		ejemploOperadorTernario();
 	}
 	
 	
@@ -25,13 +22,13 @@ public class Ejemplo01_Condicionales {
 	 * Cómo utilizar simplemente un operador if
 	 */
 	public static void primerEjemploCondicionalSimple () {
-		int numero = Utils.obtenerNumeroAzar(); // Pido un número entre 0 y 100
+		int numero = (int) Math.round(Math.random() * 100); // Pido un número entre 0 y 100
 		System.out.println(numero); // Lo imprimo
 		
 		// Determino si el número está entre unos límites
 		if ( (numero >= 10 && numero <= 50) || (numero >= 70 && numero <= 90) ) {
 			System.out.println("Bingooooooooo");
-			}
+		}
 	}
 	
 	
@@ -39,7 +36,7 @@ public class Ejemplo01_Condicionales {
 	 * Ejemplo de operador if con else	
 	 */
 	public static void ejemploCondicionalCompuesto () {
-		int numero = Utils.obtenerNumeroAzar(); // Obtengo número al azar
+		int numero = (int) Math.round(Math.random() * 100); // Obtengo número al azar
 		System.out.println("Var número: " + numero);
 		
 		// Determino si es mayor de 50 o no lo es
@@ -56,19 +53,8 @@ public class Ejemplo01_Condicionales {
 	 * Los condicionales pueden anidarse unos dentro de otros
 	 */
 	public static void ejemploCondicionalAnidado () {
-//		int numero = Utils.obtenerNumeroAzar(); // Pido número al azar
-		int numero = 50;
+		int numero = (int) Math.round(Math.random() * 100); // Pido número al azar
 		System.out.println("Var número: " + numero);
-		
-		if (numero == 50) {
-			System.out.println("lo que sea");
-		}
-		else {
-			if (numero < 50) {
-				System.out.println("lo que sea");
-			}		
-		}
-		
 		
 		// Con un condicional anidado determino muchas posibles situaciones
 		if (numero > 50) { // El número es mayor de 50
@@ -89,7 +75,7 @@ public class Ejemplo01_Condicionales {
 	 * Ejemplo de switch
 	 */
 	public static void ejemploCondicionalMultiple () {
-		int nota = 20;
+		int nota = 10;
 		
 		// switch decidirá el camino a tomar en función del valor
 		// de la variable
@@ -132,7 +118,7 @@ public class Ejemplo01_Condicionales {
 		boolean esValorPositivo;
 		
 		// Forma habitual de asignar valor a una variable en función de una condición
-		valor = 1;
+		valor = 15;
 		if (valor >= 0) {
 			esValorPositivo = true;
 		}
@@ -141,8 +127,7 @@ public class Ejemplo01_Condicionales {
 		}
 		
 		// Otra forma de hacerlo, a través del operador ternario
-		esValorPositivo = (valor >= 0)? true : false ;
-			
+		esValorPositivo = (valor >= 0)? true : false ;	
 	}
 	
 }
--- src/tutorialJava/capitulo2_condicionales/EjemploMenu.java
+++ src/tutorialJava/capitulo2_condicionales/EjemploMenu.java
@@ -1,11 +1,13 @@
 package tutorialJava.capitulo2_condicionales;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class EjemploMenu {
 
 	public static void main(String[] args) {
-		String str;
+		Scanner sc = new Scanner(System.in);
 		int opcion, numero1, numero2;
 		
 		System.out.println("Menú:");
@@ -14,13 +16,13 @@ public class EjemploMenu {
 		System.out.println("3.- Multiplicación");
 		System.out.println("4.- División");
 		
-		str = JOptionPane.showInputDialog("Introduzca la opción: ");
-		opcion = Integer.parseInt(str);
+		System.out.println("Introduzca la opción: ");
+		opcion = sc.nextInt();
 		
-		str = JOptionPane.showInputDialog("Introduzca el primer número: ");
-		numero1 = Integer.parseInt(str);
-		str = JOptionPane.showInputDialog("Introduzca el segundo número: ");
-		numero2 = Integer.parseInt(str);
+		System.out.println("Introduzca el primer número: ");
+		numero1 = sc.nextInt();
+		System.out.println("Introduzca el segundo número: ");
+		numero2 = sc.nextInt();
 		
 		
 		switch(opcion) {
@@ -29,6 +31,12 @@ public class EjemploMenu {
 			break;
 		case 2:
 			System.out.println("La resta vale: " + (numero1 - numero2));
+			break;
+		case 3:
+			System.out.println("La multiplicación vale: " + (numero1 * numero2));
+			break;
+		case 4:
+			System.out.println("La división vale: " + (numero1 / numero2));
 			break;
 		}
 	}
