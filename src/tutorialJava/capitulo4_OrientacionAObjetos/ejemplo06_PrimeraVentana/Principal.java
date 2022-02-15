f03acd3b66928330987d07ecb2e3872252efbc46
rafaelMunoz77 <rafael.munoz77@gmail.com>
2022-02-15 09:58:33 +0100

    Ejemplo 6 - Capítulo 4

--- src/tutorialJava/capitulo4_OrientacionAObjetos/ejemplo06_PrimeraVentana/Principal.java
+++ src/tutorialJava/capitulo4_OrientacionAObjetos/ejemplo06_PrimeraVentana/Principal.java
@@ -1,12 +1,24 @@
 package tutorialJava.capitulo4_OrientacionAObjetos.ejemplo06_PrimeraVentana;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Principal {
 
 
 	public static void main(String[] args) {
 		JFrame ventana = new JFrame();
+		ventana.setBounds(0, 0, 800, 600);
+		ventana.setVisible(true);
+		
+		ventana.getContentPane().setBackground(Color.decode("#32b31b"));
+		
+		ventana.getContentPane().setLayout(new BorderLayout());
+		JTextField jtf = new JTextField();
+		ventana.getContentPane().add(jtf, BorderLayout.NORTH);
 		
 
 	}
