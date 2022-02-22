8895911c96114416fc3bda5e7f5846f9b71abcc5
rafaelMunoz77 <rafael.munoz77@gmail.com>
2022-02-22 10:30:27 +0100

    Ejemplo 07 - Capítulo 04

---
+++ src/tutorialJava/capitulo4_OrientacionAObjetos/ejemplo07_VentanaConListener/Principal.java
@@ -0,0 +1,16 @@
+package tutorialJava.capitulo4_OrientacionAObjetos.ejemplo07_VentanaConListener;
+
+import javax.swing.JFrame;
+
+public class Principal {
+
+
+	public static void main(String[] args) {
+		JFrame ventana = new JFrame("Mi segunda ventana");
+
+		ventana.setBounds(0, 0, 800, 600);
+		ventana.setVisible(true);
+		
+	}
+
+}
