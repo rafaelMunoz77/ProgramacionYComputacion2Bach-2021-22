2d53d81600245e0cbddabadb275d4c5c12235258
Rafael Munoz <rafael.munoz77@gmail.com>
2022-04-01 11:29:48 +0200

    Gestión de alumnos capítulo 8 - Hasta botones de primero y último

--- src/tutorialJava/capitulo7_InterfazGrafica/ejemplo01/GestionAlumnos.java
+++ src/tutorialJava/capitulo7_InterfazGrafica/ejemplo01/GestionAlumnos.java
@@ -3,10 +3,35 @@ package tutorialJava.capitulo7_InterfazGrafica.ejemplo01;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class GestionAlumnos {
 
 	private JFrame frame;
+	private JTextField jtfId;
+	private JTextField jtfNombre;
+	private JTextField jtfApellidos;
+	private JTextField jtfNif;
+	private JPanel panel;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
 
 	/**
 	 * Launch the application.
@@ -38,6 +63,190 @@ public class GestionAlumnos {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel = new JLabel("Gestión de alumnos");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 2;
+		gbc_lblNewLabel.insets = new Insets(15, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Id:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		frame.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		jtfId = new JTextField();
+		GridBagConstraints gbc_jtfId = new GridBagConstraints();
+		gbc_jtfId.insets = new Insets(0, 0, 5, 0);
+		gbc_jtfId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfId.gridx = 1;
+		gbc_jtfId.gridy = 1;
+		frame.getContentPane().add(jtfId, gbc_jtfId);
+		jtfId.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nombre:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		frame.getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		jtfNombre = new JTextField();
+		GridBagConstraints gbc_jtfNombre = new GridBagConstraints();
+		gbc_jtfNombre.insets = new Insets(0, 0, 5, 0);
+		gbc_jtfNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfNombre.gridx = 1;
+		gbc_jtfNombre.gridy = 2;
+		frame.getContentPane().add(jtfNombre, gbc_jtfNombre);
+		jtfNombre.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Apellidos:");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		jtfApellidos = new JTextField();
+		GridBagConstraints gbc_jtfApellidos = new GridBagConstraints();
+		gbc_jtfApellidos.insets = new Insets(0, 0, 5, 0);
+		gbc_jtfApellidos.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfApellidos.gridx = 1;
+		gbc_jtfApellidos.gridy = 3;
+		frame.getContentPane().add(jtfApellidos, gbc_jtfApellidos);
+		jtfApellidos.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("NIF:");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 0;
+		gbc_lblNewLabel_4.gridy = 4;
+		frame.getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		jtfNif = new JTextField();
+		GridBagConstraints gbc_jtfNif = new GridBagConstraints();
+		gbc_jtfNif.insets = new Insets(0, 0, 5, 0);
+		gbc_jtfNif.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfNif.gridx = 1;
+		gbc_jtfNif.gridy = 4;
+		frame.getContentPane().add(jtfNif, gbc_jtfNif);
+		jtfNif.setColumns(10);
+		
+		panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 2;
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 5;
+		frame.getContentPane().add(panel, gbc_panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton = new JButton("<<");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostrarPrimerAlumno();
+			}
+		});
+		panel.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("<");
+		panel.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton(">");
+		panel.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton(">>");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostrarUltimoAlumno();
+			}
+		});
+		panel.add(btnNewButton_3);
+	}
+
+	/**
+	 * 
+	 */
+	private void mostrarPrimerAlumno() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno order by id limit 1");
+		   
+			// Navegación del objeto ResultSet
+			if (rs.next() == true) { 
+				jtfId.setText(rs.getString("id"));
+				jtfNombre.setText(rs.getString("nombre"));
+				jtfApellidos.setText(rs.getString("apellidos"));
+				jtfNif.setText(rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * 
+	 */
+	private void mostrarUltimoAlumno() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno order by id desc limit 1");
+		   
+			// Navegación del objeto ResultSet
+			if (rs.next() == true) { 
+				jtfId.setText(rs.getString("id"));
+				jtfNombre.setText(rs.getString("nombre"));
+				jtfApellidos.setText(rs.getString("apellidos"));
+				jtfNif.setText(rs.getString("nif"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
 
 }
