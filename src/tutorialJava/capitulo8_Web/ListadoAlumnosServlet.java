4eb30f44a8e4ddb357c5e79bd9b18f7441798ed8
Rafael Munoz <rafael.munoz77@gmail.com>
2022-05-13 10:42:59 +0200

    Para Felipe

---
+++ src/tutorialJava/capitulo8_Web/ListadoAlumnosServlet.java
@@ -0,0 +1,106 @@
+package tutorialJava.capitulo8_Web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListadoAlumnosServlet
+ */
+@WebServlet("/ListadoAlumnosServlet")
+public class ListadoAlumnosServlet extends HttpServlet {
+	
+	String respuesta = "";
+
+    /**
+     * Default constructor. 
+     */
+    public ListadoAlumnosServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		for (int i = 0; i < 10; i++) {
+			response.getWriter().append(i + " ");
+		}
+		mostrarTodosAlumnos();
+		response.getWriter().append(this.respuesta);
+	}
+
+	
+	
+	
+	/**
+	 * 
+	 */
+	private void mostrarTodosAlumnos() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+			Statement s = (Statement) conexion.createStatement(); 			
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno");
+		   
+			// Navegación del objeto ResultSet
+			while (rs.next() == true) { 
+				this.respuesta += rs.getString("nombre") + "</br>";
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	
+	
+	
+	/**
+	 * 
+	 */
+	private void mostrarPrimerAlumno() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+			Statement s = (Statement) conexion.createStatement(); 			
+			ResultSet rs = s.executeQuery ("select * from alumnos.alumno order by id limit 1");
+		   
+			// Navegación del objeto ResultSet
+			if (rs.next() == true) { 
+				this.respuesta += rs.getString("nombre");
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+}
